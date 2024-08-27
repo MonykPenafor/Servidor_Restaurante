160cee9b3eb8dea1e89e66c858e3b0edcf34db0d
Monyk Paola Penafor <91429002+MonykPenafor@users.noreply.github.com>
2024-08-27 18:27:05 -0300

    ajustes no DAO, passou a retornar object, e add busca por estado e data na classe de negocio de OP

--- src/main/java/ifmt/cba/persistencia/OrdemProducaoDAO.java
+++ src/main/java/ifmt/cba/persistencia/OrdemProducaoDAO.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import ifmt.cba.dto.EstadoOrdemProducaoDTO;
+import ifmt.cba.dto.EstadoPedidoDTO;
 import ifmt.cba.entity.OrdemProducao;
+import ifmt.cba.entity.Pedido;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 
@@ -26,19 +28,34 @@ public class OrdemProducaoDAO extends DAO<OrdemProducao>{
 		return ordemProducao;
 	}
 
-    @SuppressWarnings("unchecked")
-	public List<OrdemProducao> buscarPorEstado(EstadoOrdemProducaoDTO estado) throws PersistenciaException {
-		List<OrdemProducao> listaOrdemProducao = null;
-		try {
-			Query query = this.entityManager
-					.createQuery("SELECT op FROM OrdemProducao op WHERE UPPER(op.estado) = :pEstado");
-			query.setParameter("pEstado", listaOrdemProducao);
-			listaOrdemProducao = query.getResultList();
-		} catch (Exception ex) {
-			throw new PersistenciaException("Erro na selecao por estado - " + ex.getMessage());
-		}
-		return listaOrdemProducao;
-	}
+    // @SuppressWarnings("unchecked")
+	// public List<OrdemProducao> buscarPorEstado(EstadoOrdemProducaoDTO estado) throws PersistenciaException {
+	// 	List<OrdemProducao> listaOrdemProducao = null;
+	// 	try {
+	// 		Query query = this.entityManager
+	// 				.createQuery("SELECT op FROM OrdemProducao op WHERE UPPER(op.estado) = :pEstado");
+	// 		query.setParameter("pEstado", listaOrdemProducao);
+	// 		listaOrdemProducao = query.getResultList();
+	// 	} catch (Exception ex) {
+	// 		throw new PersistenciaException("Erro na selecao por estado - " + ex.getMessage());
+	// 	}
+	// 	return listaOrdemProducao;
+	// }
+
+	@SuppressWarnings("unchecked")
+    public List<OrdemProducao> buscarPorEstado(EstadoOrdemProducaoDTO estado) throws PersistenciaException {
+        List<OrdemProducao> listaOrdemProducao = null;
+        try {
+            Query query = this.entityManager
+                    .createQuery("SELECT op FROM OrdemProducao op WHERE UPPER(op.estado) = :pEstado");
+            query.setParameter("pEstado", estado.toString().toUpperCase());
+            listaOrdemProducao = query.getResultList();
+        } catch (Exception ex) {
+            throw new PersistenciaException("Erro na selecao por estado - " + ex.getMessage());
+        }
+        return listaOrdemProducao;
+    }
+
 
     @SuppressWarnings("unchecked")
 	public List<OrdemProducao> buscarPorDataProducao(LocalDate dataInicial, LocalDate dataFinal) throws PersistenciaException {
@@ -54,5 +71,25 @@ public class OrdemProducaoDAO extends DAO<OrdemProducao>{
 		}
 		return listaOrdemProducao;
 	}
+	
+
+
+
+    @SuppressWarnings("unchecked")
+	public List<OrdemProducao> buscarPorEstadoEDataProducao(EstadoOrdemProducaoDTO estado, LocalDate dataInicial, LocalDate dataFinal) throws PersistenciaException {
+		List<OrdemProducao> listaOrdemProducao = null;
+		try {
+			Query query = this.entityManager
+					.createQuery("SELECT op FROM OrdemProducao op WHERE op.dataProducao >= :pDataInicial AND op.dataProducao <= :pDataFinal AND UPPER(op.estado) = :pEstado");
+                query.setParameter("pDataInicial", dataInicial);
+                query.setParameter("pDataFinal", dataFinal);
+                query.setParameter("pEstado", estado.toString().toUpperCase());
+			listaOrdemProducao = query.getResultList();
+		} catch (Exception ex) {
+			throw new PersistenciaException("Erro na selecao por data de producao e estado - " + ex.getMessage());
+		}
+		return listaOrdemProducao;
+	}
+	
 
 }
